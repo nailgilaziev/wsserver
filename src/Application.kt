59b4dc27ea2a705d26b022f452e1fededd4a7ef8
Nail Gilaziev <nail.gilaziev@gmail.com>
2019-06-11 20:24:46 +0300

    updated code

--- src/Application.kt
+++ src/Application.kt
@@ -20,7 +20,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module() {
     install(WebSockets) {
         pingPeriod = Duration.ofSeconds(15)
-        masking = false
+        masking = true
         timeout = Duration.ofSeconds(15)
         maxFrameSize = Long.MAX_VALUE
     }
